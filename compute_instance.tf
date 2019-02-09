e2045b75c1d71214cf54a57693ba723ed0750e7b
Shin-027 <36813108+Hamachi-06@users.noreply.github.com>
2019-02-09 21:34:19 +0900

    ADD STORGE FUCNTION CONFIG

--- compute_instance.tf
+++ compute_instance.tf
@@ -1,5 +1,5 @@
 provider "google" {
-        project = "[gcp-project]"
+        project = "[project-name]"
         region  = "asia-northeast1"
   }
 
@@ -31,11 +31,42 @@ resource "google_app_engine_application" "app-terraform" {
 }
 
 resource "google_sql_database_instance" "db-test" {
-  name = "terra-db"
-  project = "[gcp-project]"
+  name = "terra-db2"
+  project = "[project-name]"
   database_version = "MYSQL_5_7"
 
   settings {
     tier = "db-f1-micro"
   }
+}
+
+resource "google_sql_database" "database" {
+  name      = "terra-database"
+  instance  = "${google_sql_database_instance.db-test.name}"
+  charset   = "sjis"
+  collation = "sjis_japanese_ci"
+}
+
+resource "google_sql_database" "users" {
+    name = "root"
+    instance = "${google_sql_database_instance.db-test.name}"
+}
+
+resource "google_cloudfunctions_function" "cf-test" {
+    name = "terra-functions"
+    region  = "asia-northeast1"
+    trigger_http  = true
+    source_archive_bucket  = "${google_storage_bucket.bucket.name}"
+    source_archive_object  = "${google_storage_bucket_object.archive.name}"
+}
+
+resource "google_storage_bucket" "bucket" {
+  name = "cloudfunction-deploy-[project-name]"
+  location = "asia-northeast1"
+}
+
+resource "google_storage_bucket_object" "archive" {
+  name   = "file"
+  source = "./testimage.txt"
+  bucket = "${google_storage_bucket.bucket.name}"
 }

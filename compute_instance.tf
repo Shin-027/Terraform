040571fd2a83af46d50cd893635ea7816e2492a9
Shin-027 <36813108+Hamachi-06@users.noreply.github.com>
2019-02-09 20:33:40 +0900

    ADD CLOUDSQL GAE CONFIG

--- compute_instance.tf
+++ compute_instance.tf
@@ -1,27 +1,41 @@
 provider "google" {
-	project = "[PROJECT-NAME]"
-	region  = "asia-northeast1"
+        project = "[gcp-project]"
+        region  = "asia-northeast1"
   }
 
   resource "google_compute_instance" "vm_instance" {
-	name         = "gce-terraform"
-	machine_type = "f1-micro"
-	zone         = "asia-northeast1-a"
+        name         = "gce-terraform"
+        machine_type = "g1-small"
+        zone         = "asia-northeast1-b"
 
-	boot_disk {
-	  initialize_params {
-		size  = 10
-		type  = "pd-standard"
-		image = "debian-cloud/debian-9"
-	  }
-	}
+        boot_disk {
+          initialize_params {
+                size  = 10
+                type  = "pd-standard"
+                image = "debian-cloud/debian-9"
+          }
+        }
 
-	network_interface {
-	  network       = "default"
-	  access_config = {}
-	}
+        network_interface {
+          network       = "default"
+          access_config = {}
+        }
 
-	service_account = {
-	  scopes = ["logging-write", "monitoring-write"]
-	}
+        service_account = {
+          scopes = ["logging-write", "monitoring-write"]
+        }
   }
+
+resource "google_app_engine_application" "app-terraform" {
+  location_id = "asia-northeast1"
+}
+
+resource "google_sql_database_instance" "db-test" {
+  name = "terra-db"
+  project = "[gcp-project]"
+  database_version = "MYSQL_5_7"
+
+  settings {
+    tier = "db-f1-micro"
+  }
+}
